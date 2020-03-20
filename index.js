e0d0f9c667cc8ec7e12d39c6dac938999e763682
Lauren Doss <doss.laurene@gmail.com>
2020-03-20 12:32:16 -0700

    updated to include writing of HTML file

--- index.js
+++ index.js
@@ -1,12 +1,125 @@
-`sconst questions = [
+const inquirer = require("inquirer"); 
+const axios = require("axios"); 
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
-];
 
-function writeToFile(fileName, data) {
+
+
+function initialize(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your GitHub username?"
+          },
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of your project?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "What is the description of your project?"
+            
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Please describe installation instructions."
+    
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Please describe usage of your project."
+    
+        },
+        {
+            type: "input",
+            name: "license",
+            message: "Please include licensing information."
+    
+        },
+        {
+            type: "input",
+            name: "contributors",
+            message: "Please include others who have contributed to your project."
+    
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Please provide an example for how to run your code."
+    
+        },
+       
+    
+    ]).then(function(inquirerResponse){
+        generateHTML(inquirerResponse){
+            return `<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>README</title>
+            </head>
+            <body>
+                <div class="jumbotron jumbotron-fluid">
+                    <div class="container">
+                      <h1 class="display-4">${answers.title}</h1>
+                      <p class="lead">${answers.description}</p>
+                    </div>
+                  </div>
+                  <div class="list-group">
+                      <ul>
+                      <h2>Table of Contents</h2>
+                    <li><a href="#Installation" class="list-group-item list-group-item-action"style="text-decoration: none;">
+                      Installation
+                    </a></li>
+                    <li><a href="#Usage" class="list-group-item list-group-item-action" style="text-decoration: none;">Usage</a></li>
+                    <li><a href="#License" class="list-group-item list-group-item-action"style="text-decoration: none;">License</a></br></li>
+                    <li><a href="#Contributors" class="list-group-item list-group-item-action"style="text-decoration: none;">Contributors</a></li>
+                    <li><a href="#Tests" class="list-group-item list-group-item-action"style="text-decoration: none;">Tests</a></li>
+                    <li><a href="#Questions" class="list-group-item list-group-item-action"style="text-decoration: none;">Questions</a></li>
+                      </ul>
+                  </div>
+                  <div>
+                    <h3><span class="badge badge-secondary" id="Installation">Installation</span></h3>
+                    <p>${answers.installation}</p>
+                  </div>
+                  <div>
+                    <h3><span class="badge badge-secondary" id="Usage">Usage</span></h3>
+                    <p>${answers.usage}</p>
+                  </div>
+                  <div>
+                    <h3><span class="badge badge-secondary" id="License">License</span></h3>
+                    <p>${answers.license}</p>
+                  </div>
+                  <div>
+                    <h3><span class="badge badge-secondary" id="Contributors">Contributors</span></h3>
+                    <p>${answers.contributors}</p>
+                  </div>
+                  <div>
+                    <h3><span class="badge badge-secondary" id="Tests">Tests</span></h3>
+                    <p>${answers.tests}</p>
+                  </div>
+                  <div>
+                    <h3><span class="badge badge-secondary" id="Questions">Questions</span></h3>
+                    <p>{gitHub photo}</p>
+                    <p>For any questions, please feel free to reach out. I can be reached at: {github email}</p>
+                  </div>
+            </body>
+            </html>`; 
+        }
+        //fs.writeFile("data.js", inquirerResponse, )
+    }); 
 }
 
-function init() {
 
-}
+axios 
+.get()
 
-init();
+
+initialize()
